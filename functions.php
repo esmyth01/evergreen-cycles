3f5bce95f13fa8090b9090b0ba8fc791eaec5b8e
esmyth01 <evsmyth@yahoo.com>
2015-04-29 13:32:06 -0700

    added functions.php and screenshot.png to evergreen theme

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Theme Name: evergreen-cycles
+Theme URI: http://evansmyth.com/wordpress/themes/evergreen-cycles
+Author: Evan Smyth
+Author URI: http://evansmyth.com
+Description: This is the theme for my web170 wordpress course at Seattle Central College
+Version: 1.0
+*/
+
+
+
+
+
+
+
+
+
+?>
