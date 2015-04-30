cebc0017373283eb4a005801e8ba14801feb2f03
esmyth01 <evsmyth@yahoo.com>
2015-04-29 22:03:10 -0700

    added wp_head and wp_footer functions

--- index.php
+++ index.php
@@ -17,6 +17,12 @@
   });
 </script>
 
+<!--Start wp head-->
+
+<?php wp_head()?>
+
+
+<!--End wp head-->
 
 </head>
 
@@ -41,10 +47,18 @@
 </nav>
 <div id="live">
 <section>
-<h2 id="about-page">About</h2>
 <div id="about">
-<img src="<?php bloginfo('template_directory'); ?>/images/about.png" alt="about pic" class="about" />
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen booLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+
+<?php if (have_posts()) : while(have_posts()) : the_post(); //start loop ?>
+
+<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<?php the_content(); ?>
+
+
+<?php endwhile; endif;  //end loop?>
+
+
 </div>
 </section>
 
@@ -85,6 +99,8 @@
 
 </div>
 </div>
+
+<?php wp_footer()?>
 
 </body>
 
