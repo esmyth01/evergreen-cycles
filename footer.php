6fe25ee3596f8b09c78295442c6adf13606b49d9
esmyth01 <evsmyth@yahoo.com>
2015-05-14 17:27:46 -0700

    Added a footer and header include to theme

---
+++ footer.php
@@ -0,0 +1,22 @@
+</div><!--end main-content-->
+
+<footer>
+<ul>
+<li>Copyright 2014 &copy;</li>
+<li>All Rights Reserved</li>
+<li><a href="index.html">Evergreen Cycles and Repair</a></li>
+<li><a href="http://validator.w3.org/check?uri=http%3A%2F%2Fedison.seattlecentral.edu%2F~esmyth01%2Fweb120%2Ffinal%2Findex.html&charset=%28detect+automatically%29&doctype=Inline&group=0">Valid HTML</a></li>
+<li><a href="http://jigsaw.w3.org/css-validator/check/referer">Valid CSS</a></li>
+
+</ul>
+</footer>
+
+</div><!--end wrapper-->
+
+<?php wp_footer()?>
+
+</body>
+
+
+
+</html>
---
+++ header.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, minimumscale=
+1.0, maximum-scale=1.0" />
+<title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?> </title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" >
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/flexslider.css" type="text/css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/jquery.flexslider.js"></script>
+<!-- Place in the <head>, after the three links -->
+<script type="text/javascript" charset="utf-8">
+  $(window).load(function() {
+    $('.flexslider').flexslider();
+  });
+</script>
+
+<script>
+$(function(){
+  $('a').each(function() {
+    if ($(this).prop('href') == window.location.href) {
+      $(this).addClass('current');
+    }
+  });
+});
+</script>
+
+<!--Start wp head-->
+
+<?php wp_head()?>
+
+
+<!--End wp head-->
+
+</head>
+
+
+
+<body <?php body_class();?>>
+
+<div id="wrapper">
+
+
+
+
+<header>
+  <div id="logo">
+<a href="http://www.evansmyth.com/wordpress/"><img src="<?php bloginfo('template_directory'); ?>/images/evergreen-new-logo(wordpress).png" alt="header" class="header"/></a>
+  </div>
+
+</header>
+
+
+<!--<nav class="main">
+<ul class="main">
+<li class="main"><a href="index.php">About</a></li>
+<li class="main"><a href="products.php">Products</a></li>
+<li class="main" ><a href="services.php">Services</a></li>
+<li class="main"><a href="training.php">Training</a></li>
+<li class="main" ><a href="blog.php">Blog</a></li>
+<li class="main" ><a href="contact.php">Contact</a></li>
+</ul>
+</nav>
+-->
+
+<?php wp_nav_menu(); ?>
+
+<div id="main-content">
