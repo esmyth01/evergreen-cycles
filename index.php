020403e3bdf2f5ec2a0a7cfb4d98fbe1950da521
esmyth01 <evsmyth@yahoo.com>
2015-06-11 18:00:31 -0700

    Created custom post feeds with featured image
    I know this is late, not sure how I skipped over this assignment. One
    of those quarters I guess.

--- index.php
+++ index.php
@@ -5,10 +5,22 @@
 
 <?php if (have_posts()) : while(have_posts()) : the_post(); //start loop ?>
 
+<article class="post-excerpt">
+
 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-<?php the_content(); ?>
+<small>
+
+Posted on <?php the_time('F j, Y'); ?>
+by <?php the_author(); ?>
+in <?php the_category(', '); ?>
+
+</small>
+<?php the_post_thumbnail('thumbnail'); ?>
+<?php the_excerpt(); ?>
 
 
+
+</article>
 <?php endwhile; endif;  //end loop?>
 
 </section>
@@ -56,12 +68,5 @@
 </div>
 
 <small>index.php</small>
-
-
-
-
-
-
-
 
 <?php get_footer(); ?>
